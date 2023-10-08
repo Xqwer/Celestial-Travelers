268048ebe5b2425b45e2741e4a56dbd0058e02f1
Xqwer <alexandergiotas@gmail.com>
2023-10-08 15:52:43 +0300

    Fixed autoplay

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,9 +6,15 @@ import { Router } from '@angular/router';
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
 
   constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    const videoRef = document.getElementById('myVideo');
+    (videoRef as any).muted = true;
+    (videoRef as any).play();
+  }
 
   goToPlanning() {
     this.router.navigate(['mission_planning'])
